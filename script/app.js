53edbb56961e9725898df8c0ea3db5f028e4816f
Anton Kameniev <anton.kameniev@chisw.com>
2021-11-05 20:16:49 +0200

    beta with name, pass, country custom validation

--- script/app.js
+++ script/app.js
@@ -1,99 +1,80 @@
 'use strict'
-const userForm = document.querySelector('.form-container');
-const selected = document.querySelector('select');
-const formPass = document.getElementById('user_pass');
-const inputs = document.getElementsByTagName('input');
-const passEyeIcon = document.querySelector('.pass_visibility');
-const passInput = document.getElementById('user_pass');
-const formBtn = document.getElementById('form_button')
-const countryArrow = document.querySelector('.country_arrow');
-let inputsArr = Array.from(inputs);
+const submitBtn = document.querySelector('#form_button')
+const passEye = document.querySelector('.fa-eye');
+const nameInput = document.querySelector('#user_name')
+const passInput = document.querySelector('#user_pass')
+const countryInput = document.querySelector('#user_country')
 
+let passReg = new RegExp ('(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}');
+let nameReg = new RegExp ('(?=.*[a-z])(?=.*[A-Z]).{3,}');
+let countryReg = /[^Country]/;
 
-inputsArr.forEach(item => {
-    if (item.placeholder == 'Name') {
-        item.addEventListener('input', () => {
-            if (item.value !== '') {
-                item.classList.add('input__active')
-                document.querySelector('.form-container_name').classList.remove('required_field')
-            } else {
-                item.classList.remove('input__active')
-                document.querySelector('.form-container_name').classList.add('required_field')
-            }
-        })
-    }    
-})
+// function requiredMessageConstuctor (elementPadding, messageInnerHTML, messageClass) {
+    
+//     this.closest('div').style.paddingBottom = `${elementPadding}`
+//     let validationMessage = document.createElement('span');
+//     validationMessage.classList.add('validation-message');
+//     validationMessage.innerHTML = `${messageInnerHTML}`
+//     this.closest('div').append(validationMessage);
+//     this.closest('div').classList.add(`${messageClass}`);
+// }
 
-
-let regCheck = new RegExp ('(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}');
-function isValid(str){
-    return regCheck.test(str);
-}
-formPass.onblur = function() {
-    if (isValid(this.value)) {
-        this.classList.add('input__active')
-        document.querySelector('.form-container_pass').classList.remove('required_field')
-    }
-}
-
-passEyeIcon.addEventListener('click', () => {
-    if (passInput.type === 'password') {
-        passInput.type = 'text';
-        passEyeIcon.classList.add('pass_visibility__hidden')
+function validationCheck (item, validator) {
+        console.log(this);
+    if (validator.test(item.value)) {
+        item.closest('div').lastChild.innerHTML = '';
+        item.closest('div').style.paddingBottom = '';
+        item.closest('div').classList.remove('required_field');
+        return validator.test(item.value)
     } else {
-        passInput.type = 'password'
-        passEyeIcon.classList.remove('pass_visibility__hidden')
-    };
-})
-
-
-selected.addEventListener('click', (e) => {
-    if (selected.value !== 'Country') {
-        selected.classList.add('input__active');
-        document.querySelector('.form-container_country').classList.remove('required_field')
+        if (item.closest('div').lastElementChild.classList.value !== 'validation-message') {
+            if (item.id === 'user_name') {
+                item.closest('div').style.paddingBottom = '7px'
+                let validationMessage = document.createElement('span');
+                validationMessage.classList.add('validation-message');
+                validationMessage.innerHTML = `Minimum 3 letters <br> Only letters`;
+                item.closest('div').append(validationMessage);
+                item.closest('div').classList.add('required_field');
+            }
+            if (item.id === 'user_pass') {
+                    item.closest('div').style.paddingBottom = '18px'
+                    let validationMessage = document.createElement('span');
+                    validationMessage.classList.add('validation-message');
+                    validationMessage.innerHTML = `<ul><li>1 uppercase</li><li>1 capital letter</li><li>minimum 8 characters</li></ul>`;
+                    item.closest('div').append(validationMessage);
+                    item.closest('div').classList.add('required_field');
+            }
+            if (item.id === 'user_country') {
+                let validationMessage = document.createElement('span');
+                validationMessage.classList.add('validation-message');
+                validationMessage.innerHTML = `Cuontry field is required`;
+                item.closest('div').append(validationMessage);
+                item.closest('div').classList.add('required_field');
+            }
+        }                
+        return validator.test(item.value)
     }
-    countryArrow.classList.toggle('country_arrow__rotate')
-})
+}
 
 
 
-const checkForms = () => {    
-    let bodyObj = {
-        text: false,
-        userPass: false,
-        radio: false,
-        checkbox: false,
-        country: false
-    };
-    let result = true;
-    inputsArr.map(item => {
-        if (item.type === 'text' && item.value.length >= 3) {
-            bodyObj[item.type] = true;
-        }
-        if (item.name === 'userPass' && isValid(item.value)) {
-            bodyObj[item.name] = true;      
-            }
-        if (item.type === 'radio' && item.checked) {
-            bodyObj[item.type] = true;  
-            }
-        if (item.type === 'checkbox' && item.checked) {
-            bodyObj[item.type] = true;       
-            }
+const eaePassTypeToggler = () => {
+    passEye.addEventListener('click', ()=> {
+        passEye.classList.toggle('fa-eye-slash');
+        if (passInput.type === 'password') {
+            passInput.type = 'text'
+        } else {
+            passInput.type = 'password'
+        }        
     })
-    if (selected.value !== 'Country') {
-        bodyObj.country = true;
-    }
-    Object.values(bodyObj).forEach(item => item !== true ? result = false : result)
-    return result;
 }
 
+eaePassTypeToggler();
 
-document.addEventListener('input', (e)=> {
-    if (checkForms()) {
-        formBtn.classList.add('form_button__active');
-    } else {
-        formBtn.classList.remove('form_button__active');
-    }
+
+submitBtn.addEventListener('click', ()=> {
+    validationCheck(nameInput, nameReg);
+    validationCheck(passInput, passReg);
+    validationCheck(countryInput, countryReg);
 })
-
 
